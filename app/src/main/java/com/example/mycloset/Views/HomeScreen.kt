6cb2efbff354793bd4eabf104ea9c6e89b2aa648
Nafisul Nazrul <nafisul.tushar@gmail.com>
2023-10-11 14:49:56 +0300

    Changed navigation and added SingleItemScreen

--- app/src/main/java/com/example/mycloset/Views/HomeScreen.kt
+++ app/src/main/java/com/example/mycloset/Views/HomeScreen.kt
@@ -3,14 +3,18 @@
 package com.example.mycloset.Views
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Camera
+import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,8 +27,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.mycloset.data.SignupViewModel
+import com.example.mycloset.navigation.Screen
+
 //import com.example.mycloset.Screen
 
 
@@ -64,28 +72,42 @@ fun EmptyHomeScreen() {
     }
 }
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(navController: NavController) {
-    Scaffold(topBar = {
-        TopAppBar(
-//                colors = topAppBarColors(
-//                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-//                    titleContentColor = MaterialTheme.colorScheme.primary,
-//                ),
-            title = {
-                Text("MyCloset", modifier = Modifier.padding(2.dp))
-            }, actions = {
-                IconButton(
-                    onClick = { /* Navigate to the scanner screen
-                    navController.navigate(route = Screen.Camera.route)*/}
-                ) {
-                    Icon(Icons.Default.Camera, contentDescription = null)
+
+fun HomeScreen() {
+    var viewModel: SignupViewModel = viewModel()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "MyCloset",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                },
+                actions = {
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(Icons.Default.Logout, contentDescription = "Logout")
+                    }
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            viewModel.logout()
+                        }
+                    ) {
+                        Icon(Icons.Default.Camera, contentDescription = "Camera")
+                    }
                 }
-            }
-        )
-    }) { padding ->
+            )
+        }
+    ) { padding ->
         LazyVerticalGrid(
             columns = GridCells.Fixed(2), modifier = Modifier
                 .fillMaxSize()
@@ -102,7 +124,7 @@ fun HomeScreen(navController: NavController) {
 @Composable
 fun PrevHome() {
     HomeScreen(
-        navController = rememberNavController()
+
     )
 }
 
